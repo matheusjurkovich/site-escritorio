23944dd1d898e246fab7efdb52b0fb7bbe46dcde
Matheus Jurkovich <77711064+matheusjurkovich@users.noreply.github.com>
2022-07-01 17:42:23 -0300

    ajuste accordion

--- main.js
+++ main.js
@@ -80,5 +80,19 @@ ScrollReveal({
   .contact-alternative,
   #faq,
   #faq .accordion,
-  #faq .accordion-item
-  `)
+  #faq .accordion-item`)
+
+var acc = document.getElementsByClassName("accordion");
+
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
